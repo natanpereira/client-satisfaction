ec4501bb70031c27bb2e9b5f2e56dc319f0246e2
Natan Pereira <natanj49@gmail.com>
2024-07-17 00:42:32 -0300

    feat: renamed resources

--- src/client-satisfaction/dto/update-client-satisfaction.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateClientSatisfactionDto } from './create-client-satisfaction.dto';
-
-export class UpdateClientSatisfactionDto extends PartialType(
-  CreateClientSatisfactionDto,
-) {}
